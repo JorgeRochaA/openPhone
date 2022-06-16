3898ae595e153ef36d68f11b069503905ff58559
Jorge Rocha <79389355+JorgeRochaA@users.noreply.github.com>
2022-06-15 20:33:59 -0600

    feat: add navbar (#1)
    * feat: starting navbar
    
    * feat: add mouse effect
    
    * feat: add navbar

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+//components
+import Navbar from "../components/Navbar";
+//styles
 import "../styles/Home/Home.scss";
 function Home() {
-  return <div className="home">Home</div>;
+  const cursor = useRef(null);
+  const positionRef = useRef({
+    mouseX: 0,
+    mouseY: 0,
+  });
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (event) => {
+      const { clientX, clientY } = event;
+
+      const mouseX = clientX;
+      const mouseY = clientY;
+
+      positionRef.current.mouseX = mouseX - cursor.current.clientWidth / 2;
+      positionRef.current.mouseY = mouseY - cursor.current.clientHeight / 2;
+      cursor.current.style.transform = `translate3d(${
+        mouseX - cursor.current.clientWidth / 2
+      }px, ${mouseY - cursor.current.clientHeight / 2}px, 0)`;
+    });
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="light" ref={cursor}></div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Home;
