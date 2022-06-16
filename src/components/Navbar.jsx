df82f665cbdc34c5be7ef738e742b9ff71a8ccc5
JorgeRochaA <jor2019.cr@gmail.com>
2022-06-15 20:32:41 -0600

    feat: add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar/Navbar.scss";
 function Navbar() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
     <div className="navbar">
       <div className="logo_container">
         <img src={require("../assets/logo.png")} alt="logo" />
-        <a className="option_1" href="#Pricing">
-          Pricing
-        </a>
-        <a className="option_1" href="#About">
-          About
-        </a>
-        <a className="option_1" href="#Downloads">
-          Downloads
-        </a>
-        <div>
-          <a className="option_1" href="#Careers">
-            Careers
-          </a>
-          ✨
+        <div
+          className={menuIsOpen ? "bars_container open" : "bars_container"}
+          onClick={toggleMenu}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
         </div>
       </div>
-      <div className="options_container">
-        <a className="option_2" href="#Help">
-          Help
-        </a>
-        <a className="option_2" href="#LogIn">
-          Log in
-        </a>
-        <a className="option_2" href="#SignUp">
-          Sign up
-        </a>
+
+      <div
+        className={menuIsOpen ? "options_container open" : "options_container"}
+      >
+        <div className="options_1_container">
+          <a className="option_1" href="#Pricing">
+            Pricing
+          </a>
+          <a className="option_1" href="#About">
+            About
+          </a>
+          <a className="option_1" href="#Downloads">
+            Downloads
+          </a>
+          <div className="careers_link">
+            <a className="option_1" href="#Careers">
+              Careers
+            </a>
+            ✨
+          </div>
+        </div>
+        <div className="options_2_container">
+          <a className="option_2" href="#Help">
+            Help
+          </a>
+          <a className="option_2" href="#LogIn">
+            Log in
+          </a>
+          <a className="option_2" href="#SignUp">
+            Sign up
+          </a>
+        </div>
       </div>
     </div>
   );
