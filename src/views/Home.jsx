e473e9d34b1436a7335e8768508eb13b4ebc3dbb
JorgeRochaA <jor2019.cr@gmail.com>
2022-06-16 23:59:57 -0600

    feat: add cards section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react";
 import Navbar from "../components/Navbar";
 import MainSection from "../components/MainSection";
 import HowItWorksSection from "../components/HowItWorksSection";
+import CardsSection from "../components/CardsSection";
 //styles
 import "../styles/Home/Home.scss";
 function Home() {
@@ -33,6 +34,7 @@ function Home() {
       <Navbar />
       <MainSection />
       <HowItWorksSection />
+      <CardsSection />
     </div>
   );
 }
